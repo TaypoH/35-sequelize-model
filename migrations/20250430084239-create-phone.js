b62ef3c23c7e24310b46c7287ec63c1d29b866bb
TaypoH <merkantiln@gmail.com>
2025-05-01 11:36:38 +0300

    Fix phone migration

--- migrations/20250430084239-create-phone.js
+++ migrations/20250430084239-create-phone.js
@@ -1,46 +1,52 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Phones', {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('phones', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       brand: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       model: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
-      manufacturedYear: {
-        type: Sequelize.SMALLINT
+      manufactured_year: {
+        type: Sequelize.SMALLINT,
       },
-      ramSize: {
-        type: Sequelize.SMALLINT
+      ram_size: {
+        type: Sequelize.SMALLINT,
+        allowNull: false,
       },
       cpu: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
-      screenDiagonal: {
-        type: Sequelize.FLOAT
-      },
-      isNfc: {
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
+      screen_diagonal: {
+        type: Sequelize.FLOAT,
         allowNull: false,
-        type: Sequelize.DATE
       },
-      updatedAt: {
+      is_nfc: {
+        type: Sequelize.BOOLEAN,
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        defaultValue: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
     });
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Phones');
-  }
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('phones');
+  },
 };
